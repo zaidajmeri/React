dc4ebf4a520af8cc42ca1f6e89e043096dc50757
Zaid Ajmeri <111185630+zaidajmeri@users.noreply.github.com>
2024-02-06 22:52:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ backgroundColor: 'yellow', textAlign: 'center' }}>
+      <h1><i>Hello React | Zaid Ajmeri</i></h1>
+    </div>
+  );
+}
+
+export default App;
